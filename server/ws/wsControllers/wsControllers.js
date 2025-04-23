0d4919c0af92a44ce44e088c37a828df69cfc816
myounasfaisal <younasf69@gmail.com>
2025-04-23 13:09:08 +0500

    Indented and removed console log statement

--- server/ws/wsControllers/wsControllers.js
+++ server/ws/wsControllers/wsControllers.js
@@ -1,20 +1,20 @@
-export const handleMessage=(bytes,uuid,users,connections)=>{
-    const message=JSON.parse(bytes.toString());
-    const user=users[uuid];
-    user.state=message;
-    broadcastUsers(connections,users);
-    console.log();
+
+export const handleMessage = (bytes, uuid, users, connections) => {
+    const message = JSON.parse(bytes.toString());
+    const user = users[uuid];
+    user.state = message;
+    broadcastUsers(connections, users);
 }
 
-export const handleClose=(uuid,connections,users)=>{
-delete connections[uuid];
-delete users[uuid];
+export const handleClose = (uuid, connections, users) => {
+    delete connections[uuid];
+    delete users[uuid];
 };
 
-export const broadcastUsers=(connections,users)=>{
-        Object.keys(connections).forEach(uuid=>{
-        const connection=connections[uuid];
-        const message=JSON.stringify(users);
+export const broadcastUsers = (connections, users) => {
+    Object.keys(connections).forEach(uuid => {
+        const connection = connections[uuid];
+        const message = JSON.stringify(users);
         connection.send(message);
     })
 }
